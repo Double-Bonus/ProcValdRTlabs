cf6a9c2c3d3fbf270a4d8b7c6cea84f5a3a2ae32
unknown <zygimantasmarma@gmail.com>
2021-02-09 22:52:32 +0200

    done 1 lab

--- lab1-optimizavimas/optimize_cost_f.m
+++ lab1-optimizavimas/optimize_cost_f.m
@@ -1,12 +1,8 @@
 function [price, radius] = optimize_cost_f(volume, metal_price, labor_cost)
     X = 0.01:0.001:1; %radius array
-    fprintf('labor_cost: %f\n', labor_cost);
-    fprintf('metal_price: %f\n', metal_price);
-
-    %Kainos
-    total_labor = ((4*pi.*X) + volume./(pi * X.^2) ) .* labor_cost;
-    total_metal = metal_price.*(2*pi.*X.^2 + ((volume*2) ./ X));
-    P = total_labor + total_metal;
+    total_labor = (4*pi.*X) + volume./(pi * X.^2);
+    total_metal = 2*pi.*X.^2 + ((volume*2) ./ X);
+    P = total_labor .* labor_cost  + total_metal .* metal_price;
     [price, p_inx] = min(P);
     radius = X(p_inx);
 end
