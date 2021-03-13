56849c5bacc5dafa39b3bd6be98fdf430923c713
Zygimantas <zygimantasmarma@gmail.com>
2021-03-13 22:46:37 +0200

    rewrote 4 LB

--- lab4-reaction/mainLab4.m
+++ lab4-reaction/mainLab4.m
@@ -1,107 +1,121 @@
-close all; 
-clc, clear
+clc, clear, close all;
 
-OFFSET = 2;
+OFFSET_X = 1.5; % for drawing
 
-transfer_fnc = tf(2.25,[1 0.5 2.25])         % define closed-loop transfer fn 
+transfer_fnc = tf(2.25,[1 0.5 2.25])        % define closed-loop transfer fn 
 % 'Response Characteristics'
-P=pole(transfer_fnc)                 % find closed-loop poles
-Pre = abs(real(P(1)));                % find real part of pole
-Pim = abs(imag(P(1)));            % find imag part of pole
-frequency_wn = sqrt(Pre*Pre+Pim*Pim)       % calc natural frequency 
-damping_ratio=(Pre/frequency_wn)         % calc damping ratio
-overshoot_prct = (exp(-1*Pre*pi/Pim))*100   % calc percent overshoot
-
-peak_time=pi/Pim           % calc peak time 
-Ts=4/Pre       % calc settling time  (0.02 indicator) 
-
+P = pole(transfer_fnc);                       % find closed-loop poles
+Pre = abs(real(P(1)));                      % find real part of pole
+Pim = abs(imag(P(1)));                      % find imag part of pole
+frequency_wn = sqrt(Pre*Pre+Pim*Pim);       % calc natural frequency 
+damping_ratio=(Pre/frequency_wn) ;          % calc damping ratio
+overshoot_prct = (exp(-1*Pre*pi/Pim))*100 ; % calc percent overshoot
+peak_time = pi/Pim;                         % calc peak time 
+Ts = 4/Pre;                                   % calc settling time  (0.02 indicator) 
 %--------------------------------------------------------------------
-% Plot step response of T(s) using Matlab STEP function
-step(transfer_fnc)  %PLOTING
+step(transfer_fnc); % Plot step response of T(s) using Matlab STEP function
+grid on;
 %-------------------------------------------------------------------
 % Plot step response using derived (part B) expression for C(t)
-t=[0.1:0.01:25];         % define time domain
+t = 0.1:0.01:25;      % define time domain
 
-for x=1:length(t)                   % calc c(t) for each t
+for x = 1:length(t) % calc c(t) for each t [This given function by extra matterial]
     c(x)=1-1.01418*(cos(1.47902*t(x)-(9.59*pi/180)))*(exp((-0.25)*t(x)));
 end
 
+c0 = step(transfer_fnc, t)'; % MATLAB function
 
 syms s
-fun_anl= ilaplace(2.25 / (s^2 + 0.5*s + 2.25) * (1/s))
-for ii=1:length(t)                   % calc c(t) for each t
-    c2(ii) = 1 - exp(-t(ii)/4)*(cos((35^(1/2)*t(ii))/4) + (35^(1/2)*sin((35^(1/2)*t(ii))/4))/35);
+fun_anl = ilaplace(2.25 / (s^2 + 0.5*s + 2.25) * (1/s));
+for ii = 1:length(t) % calc c1(t) for each t
+    c1(ii) = 1 - exp(-t(ii)/4)*(cos((35^(1/2)*t(ii))/4) + (35^(1/2)*sin((35^(1/2)*t(ii))/4))/35);
 end
 
+peak_value = calculate_peak_value(t, c1, peak_time);
 
-% I funkcijas galime sudet situs
-peak_value = t(1);
-for ii = 1:length(c)
-    if peak_time >= t(ii)                  %CAN CHANGE
-        peak_value = c(ii);
-    end
-end
-peak_value
+[reaction_value, reaction_time, reaction_time_plot] = calculate_reaction(t, c1);
 
-
-reaction_time = 0;
-reaction_time_str = 0;
-reaction_time_plot = 0;
-reaction_value = 0;
-last_value = c(length(c));
-for ii = 1:length(c)
-    if (0 == reaction_time_str) && (c(ii) > 0.1 * last_value)                  %CAN CHANGE
-        reaction_time_str = t(ii);
-    elseif (c(ii) >= 0.9 * last_value)
-        reaction_value = c(ii)
-        reaction_time = t(ii) - reaction_time_str
-        reaction_time_plot = t(ii)
-        break; %found reaction time
-    end
-end
-
-
-settling_value = 0;
-for ii = 1:length(c)
-    if  t(ii) >= Ts
-        settling_value = c(ii);
-        break;
-    end
-end
+settling_value = get_settling_value(t, c1, Ts);
  
 
+figure(2);
+plot(t, c1);
 grid on;
-figure(2) ;              % open new plotting window
-plot(t,c)        % plot c(t)
 hold on;
-title('Analitic model function')
+title('Analitic model function');
+ylabel('Amplitude');
+ylim([0 2]);
+xlabel('Time, s');
 
-
-plot(reaction_time_plot, reaction_value, 'o'); %ADD  COLOR
-% description = 'Reaction time: ' + num2str(reaction_time_plot) + 'in s';
+plot(reaction_time_plot, reaction_value, '.y', 'MarkerSize', 30); %ADD  COLOR??????!!!!!!!!!!!!!!!!!!?
 description = sprintf("Reaction time: %0.3f", reaction_time);
-text(reaction_time_plot + OFFSET, reaction_value, description);
+text(reaction_time_plot + OFFSET_X, reaction_value, description);
 
-plot(peak_time, peak_value, 'o'); % Mp
+plot(peak_time, peak_value, '.g', 'MarkerSize', 30); % Mp
 description = sprintf("Peak ampl: %0.2f Peak time: %0.3f s; Overshoot: %2.2f  ", peak_value, peak_time, overshoot_prct);
-text(peak_time + OFFSET, peak_value, description);
+text(peak_time + OFFSET_X, peak_value, description);
 
-plot(Ts, settling_value, 'o'); %  setting time ts
+plot(Ts, settling_value, '.r', 'MarkerSize', 30); %  setting time ts
 description = sprintf("End of porcess time: %0.2f  ", Ts);
-text(Ts + OFFSET, settling_value, description);
+text(Ts + OFFSET_X, settling_value, description);
 
 
-figure(3);               % open new plotting window
-plot(t, c, t, c2);       % plot c(t)
-title('Two functions');
+figure(3);
+plot(t, c0, t, c1);
+title('Matlab and analytical models functions');
 ylabel('Amplitude');
 xlabel('Time, s');
 legend('MATLAB', 'Analytical')
+grid on;
 
-error_tr = abs(c2-c);
+
+error_tr = abs(c1-c0);
 figure(4);
 plot(t, error_tr);
 title('Error beteen Matlab and analytical models');
 ylabel('Absolute error');
 xlabel('Time, s'); 
+grid on;
+
+
+% static functions
+function p_val = calculate_peak_value(time, func, peak_time)
+    p_val = time(1);
+    for ii = 1:length(func)
+        if peak_time >= time(ii)
+            p_val = func(ii);
+        end
+    end
+end
+
+function [rctn_value, rctn_time, rctn_time_plot] = calculate_reaction(time, func)
+    rctn_time = 0;
+    reaction_time_str = 0;
+    rctn_time_plot = 0;
+    rctn_value = 0;
+    last_value = func(length(func));
+    for ii = 1:length(func)
+        if (0 == reaction_time_str) && (func(ii) > 0.1 * last_value)
+            reaction_time_str = time(ii);
+        elseif (func(ii) >= 0.9 * last_value)
+            rctn_value = func(ii);
+            rctn_time = time(ii) - reaction_time_str;
+            rctn_time_plot = time(ii);
+            break; %found reaction time
+        end
+    end
+end
+
+function s_val = get_settling_value(time, func, set_time)
+    s_val = 0;
+    for ii = 1:length(func)
+        if  time(ii) >= set_time
+            s_val = func(ii);
+            break;
+        end
+    end
+end
+ 
+
+
 
